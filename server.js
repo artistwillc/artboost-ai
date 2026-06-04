d4ad8c302f0a98e44808985b373cf1ba3a7f3ce0
Will Cooper <artistwill@yahoo.com>
2026-06-04 16:53:22 -0500

    Deploy scheduler debug version

--- server.js
+++ server.js
@@ -2370,7 +2370,7 @@ Rules:
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Pinterest API base: ${PINTEREST_API_BASE}`);
-  console.log("LIVE SERVER VERSION: X DEBUG 1");
+  console.log("LIVE SERVER VERSION: SCHEDULER DEBUG 1");
   console.log(
     `Stripe configured: ${process.env.STRIPE_SECRET_KEY ? "yes" : "no"}`
   );
