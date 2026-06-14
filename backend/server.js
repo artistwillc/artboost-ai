983040300a0f20254e25ceaf7eda340d50b3003e
artistwillc <artistwill@yahoo.com>
2026-06-14 12:40:47 -0500

    Update live server version log message

--- backend/server.js
+++ backend/server.js
@@ -2941,6 +2941,7 @@ app.listen(PORT, async () => {
 
   console.log("LIVE SERVER VERSION: INSTAGRAM LONG CAPTION FIX 1");
   console.log("LIVE SERVER VERSION: FACEBOOK PERSISTENCE 1");
+  console.log("LIVE SERVER VERSION: INSTAGRAM SCHEDULER FIX 1");
 
   console.log(
     `Stripe configured: ${
